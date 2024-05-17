352706b06d36c74f65a7e35e816fdef052fc02af
fbrodrigorezino <rodrigo.rezino@freshbooks.com>
2024-05-17 11:17:31 +0200

    Update ai explain

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python /src/main.py --openai_api_key "$1" --github_token "$2" --github_pr_id "$3" --google_gemini_token "$4" --ignore_files_with_content "$5" --ignore_files_in_paths "$6"
+python /src/main.py --openai_api_key "$1" --github_token "$2" --github_pr_id "$3" --google_gemini_token "$4" --ignore_files_with_content "$5" --ignore_files_in_paths "$6" --instructions "$7"
